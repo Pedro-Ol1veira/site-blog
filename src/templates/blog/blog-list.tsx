76b76b3f2b60d2f516cc95c8109c47c130439a18
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2026-05-22 21:40:42 -0300

    Feat: pagina de blog

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,14 +1,19 @@
 import { Search } from "@/components/search";
 import { useRouter } from "next/router";
+import { PostCard } from "./components/post-card";
+import { PostGridCard } from "./components/post-grid-card";
+import { allPosts } from '.contentlayer/generated';
 
 export function BlogList() {
   const router = useRouter();
   const query = (router.query.q as string) ?? '';
   const pageTitle = query ? `Reultados de busca para "${query}"` : 'Dicas e estratégias para impulsionar seu negócio'; 
   
+  const posts = allPosts;
+
   return (
     <div className="flex flex-col py-24 grow h-full">
-      <header className="">
+      <header className="pb-14">
         <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
             <div className="flex flex-col gap-4 md:px-0">
               <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
@@ -21,6 +26,23 @@ export function BlogList() {
             <Search />
         </div>
       </header>
+
+      <PostGridCard>
+        {posts.map((post) => (
+          <PostCard
+            key={post._id} 
+            title={post.title}
+            description={post.description}
+            date={new Date(post.date).toLocaleDateString('pt-BR')}
+            author={{
+              avatar: post.author?.avatar,
+              name: post.author?.name,
+            }}
+            image={post.image}
+            slug={post.slug}
+          />
+        ))}
+      </PostGridCard>
     </div>
   );
 }
