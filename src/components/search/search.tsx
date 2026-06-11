49df00325b724476883d7823d0fa5910e8c8bcc7
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2026-06-10 21:59:42 -0300

    Mudando pagina de 404 para app router

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,13 +1,14 @@
 import { cn } from "@/lib/utils";
 import { CircleX, SearchIcon } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export const Search = () => {
-
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const router = useRouter();
   const searchParams = useSearchParams()
   const query = searchParams?.get('q') ?? '';
+  const hasQuery = searchParams?.has('q');
 
   const handleSearch = useCallback((event: React.ChangeEvent) => {
       event.preventDefault();
@@ -31,12 +32,19 @@ export const Search = () => {
     });
   }
 
+  useEffect(() => {
+    if(hasQuery) {
+      inputRef.current?.focus();
+    }
+  }, [hasQuery])
+
     
   return (
     <form className="relative group w-full md:w-60" onSubmit={handleSearch}>
       <SearchIcon className={cn('text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300', query ? 'text-blue-300' : '')}
       />
       <input
+        ref={inputRef}
         type="text"
         placeholder="Buscar"
         onChange={handleQueryChange}
